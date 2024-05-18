daa5f5f5d1e673c81da8b78996bb6dab5b7dea25
NghiaPham777 <nghiapham2704@gmail.com>
2024-05-18 16:07:56 +0700

    Add Mui Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import Button from '@mui/material/Button'
-import './App.css'
-
+import Typography from '@mui/material/Typography';
 function App() {
 
   return (
     <>
     <div>Pham Huu Nghia</div>
+
+    <Typography variant='body' color="text.secondary">Test Typography</Typography> 
+    <br></br>
+
     <Button variant="text">Text</Button>
     <Button variant="contained">Contained</Button>
     <Button variant="outlined">Outlined</Button>  
