5e554de3422f7f16d84052692e5960e2fa60e66e
NghiaPham777 <nghiapham2704@gmail.com>
2024-05-30 09:23:04 +0700

    Add theme dard&light

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography';
+import {  useColorScheme,} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 function App() {
-
-
   return (
     <>
+    <ModeToggle></ModeToggle>
+    <hr />
     <div>Pham Huu Nghia</div>
 
     <Typography variant='body' color="text.secondary">Test Typography</Typography> 
