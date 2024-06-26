8349f83ba36fe7d5ebb873906b2c18070dd4ac2b
NghiaPham777 <nghiapham2704@gmail.com>
2024-06-26 15:17:20 +0700

    Add drag drop card (Drag over card)

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -7,17 +7,34 @@ import CommentIcon from '@mui/icons-material/Comment'
 import AttachmentIcon from '@mui/icons-material/Attachment'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 function Card({ card }) {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id: card._id,
+    data: { ...card }
+  })
+
+  const dndKitCardStyles = {
+    // touchAction: 'none',
+    // Doc là CSS.Transform sẽ lỗi kiểu streatch
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined, // làm mờ
+    border: isDragging ? '2px solid #2ecc71' : undefined
+  }
   const shouldShowCardAction = () => {
     return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachment?.length
   }
   return (
-    <MuiCard sx={{
-      cursor: 'pointer',
-      boxShadow: '0 1px 1px rgba(0, 0, 0, 0.5)',
-      overflow: 'unset'
-    }}>
+    <MuiCard 
+      ref={setNodeRef} style={dndKitCardStyles} {...attributes} {...listeners}
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.5)',
+        overflow: 'unset'
+      }}
+    >
       {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} /> }
       <CardContent sx={{ p:1.5, '&:last-child': { p:1.5 } }}>
         <Typography> {card?.title} </Typography>
